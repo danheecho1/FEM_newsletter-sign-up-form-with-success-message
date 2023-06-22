fa96b29414916322868d6e9935dbc4888a341bf8
Danny Cho <danheecho@gmail.com>
2023-06-22 01:03:02 -0700

    Updated the readme file. Changed a div tag containing an input element to a form tag. Updated CSS to match class names. Updated JS to pass data from subscribe page to confirmation page and reset input value when dismiss button is clicked.

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const subscribeButton = document.querySelector(".sign-up-card-div__info-div__email-div__button");
+const subscribeButton = document.querySelector(".sign-up-card-div__info-div__email-form__button");
 const signUpCard = document.querySelector(".sign-up-card-div");
-const confirmationCard = document.querySelector(".confirmation-card-div")
+const confirmationCard = document.querySelector(".confirmation-card-div");
+const form = document.querySelector(".sign-up-card-div__info-div__email-form");
+let input = document.querySelector(".sign-up-card-div__info-div__email-form__input");
+let email = document.querySelector(".confirmation-card-div__description span");
 
-const confirmation = () => {
+const toggle = () => {
 	signUpCard.classList.toggle("hidden");
-    confirmationCard.classList.toggle("hidden");
+	confirmationCard.classList.toggle("hidden");
+}
+
+const subscribe = () => {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+    })
+	toggle();
+    let formData = new FormData(form)
+    email.innerHTML = formData.get("email");
 };
+
+const dismiss = () => {
+	toggle();
+    input.value = "";
+}
