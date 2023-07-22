0c3f9cfbe052c0accebdff5833cd187b4c713306
Danny Cho <danheecho@gmail.com>
2023-07-21 23:57:57 -0700

    Added screenshots for the readme file. Finished css styling for the desktop view's success page. Updated JS for handling invalid email submission.

--- script.js
+++ script.js
@@ -2,21 +2,34 @@ const subscribeButton = document.querySelector(".sign-up-card-div__info-div__ema
 const signUpCard = document.querySelector(".sign-up-card-div");
 const confirmationCard = document.querySelector(".confirmation-card-div");
 const form = document.querySelector(".sign-up-card-div__info-div__email-form");
-let input = document.querySelector(".sign-up-card-div__info-div__email-form__input");
-let email = document.querySelector(".confirmation-card-div__description span");
+const input = document.querySelector(".sign-up-card-div__info-div__email-form__input");
+const indicator = document.querySelector(".sign-up-card-div__info-div__email-form__label-div__invalid-indicator");
+const email = document.querySelector(".confirmation-card-div__description span");
+const regex = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
 
 const toggle = () => {
 	signUpCard.classList.toggle("hidden");
 	confirmationCard.classList.toggle("hidden");
+    input.classList.remove("error");
+    indicator.classList.remove("error");
+}
+
+const indicateInvalidEmail = () => {
+    input.classList.add("error");
+    indicator.classList.add("error");
 }
 
 const subscribe = () => {
     form.addEventListener("submit", function(e) {
         e.preventDefault();
     })
-	toggle();
     let formData = new FormData(form)
-    email.innerHTML = formData.get("email");
+    if(!formData.get("email") || !regex.test(formData.get("email"))) {
+        indicateInvalidEmail();
+    } else {
+        email.innerHTML = formData.get("email");
+        toggle();
+    }
 };
 
 const dismiss = () => {
